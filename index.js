bf0daea0644b1a01975e860ebb709d173099fb35
kader009 <kadermollarasel000@gmail.com>
2025-03-29 12:50:27 +0600

    install and update cookieparser

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import express from "express";
 import mongodb from 'mongodb'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
-dotenv.config()
-const port = process.env.PORT
-
+import cookieParser from "cookie-parser";
 const app = express()
+const port = process.env.PORT || 5000
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+dotenv.config()
 
 
 app.use('/', (req,res) =>{
