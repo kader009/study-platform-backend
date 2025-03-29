670806d1aee682ed94124e3c42736b0d24354f46
kader009 <kadermollarasel000@gmail.com>
2025-03-29 12:59:36 +0600

    mongodb connect with database

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-import express from "express";
-import mongodb from 'mongodb'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import cookieParser from "cookie-parser";
-const app = express()
-const port = process.env.PORT || 5000
-
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-dotenv.config()
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+dotenv.config();
 
+// mongodb ulr
+const uri = process.env.DATABASE_URL;
 
-app.use('/', (req,res) =>{
-  res.send('student platform')
-})
+// mongodb client connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    await client.connect();
+    console.log('You successfully connected to MongoDB!');
+  } finally {
+  }
+}
+run().catch(console.dir);
 
-app.listen(port, () =>{
-  console.log(`Student server is on ${port}`)
-})
+app.use('/', (req, res) => {
+  res.send('student platform');
+});
+
+app.listen(port, () => {
+  console.log(`Student server is on ${port}`);
+});
