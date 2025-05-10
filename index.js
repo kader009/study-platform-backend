fbd60b23acad401a67e63f7a11b8449350c02ec0
kader009 <kadermollarasel000@gmail.com>
2025-05-10 16:41:17 +0600

    cors issue solve

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' })); 
 app.use(cookieParser());
 dotenv.config();
 
